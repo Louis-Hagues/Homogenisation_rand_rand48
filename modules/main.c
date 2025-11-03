52d3c556b37717a3b7e58e6158177dcd318150a6
Louis Hagues <louis.hagues@etu.univ-lehavre.fr>
2025-11-03 17:41:00 +0100

    correction de quelques bug sur les include

--- modules/main.c
+++ modules/main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include "traitement.h"
+
+    const int plage = 100;
 int main() {
+    int tirages = 1000000;
+    int tabResultats[plage];
+    traitement_rand(tirages, plage, tabResultats);
+    traitement_rand48(tirages, plage, tabResultats);
+
+    printf("Résultats:\n");
+    for (int i = 0; i < plage; i++) {
+        printf("Valeur %d: %d occurrences\n", i, tabResultats[i]);
+    }
+    printf("Traitement terminé.\n");
+
     return 0;
 }
 
--- modules/traitement.c
+++ modules/traitement.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+0
 // traitement avec 20 fork
 int traitement_rand(int tirages, int plage, int* tabResultats) {
     int nbTiragesFork = tirages/20;
 
     //création des forks
     for (int i = 0; i < 20; i++) {
+        printf("Fork numéro %d\n", i);
         if (fork() == 0) {
+            
             //traitement des tirages
             for (int j = 0; j < nbTiragesFork + (i < nbTiragesFork ? 1 : 0); j++) {
+                printf("Fork %d: tirage %d\n", i, j);
                 int r = rand(plage);
                 tabResultats[r]++;
             }

88bc6f525090cc1d9f1d572bc363108ea297ce38
Thomas BOUDEELE <thomas.boudeele@univ-lehavre.fr>
2025-11-24 17:44:24 +0100

    debut creation socket

--- modules/socket.c
+++ modules/socket.c
@@ -3,8 +3,20 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <netinet/in.h> 
 
 
-int create_socket(int domain, int type, int protocol) {}
+int create_socket(int domain, int type, int protocol) 
+{
+    int socket = socket(domain,type,protocol);
+
+    if(socket = -1)
+    {
+        perror("Erreur lors de la création de l'objet socket");
+    }
+    int sockopt = setsockopt(socket,SOL_SOCKET,SO_REUSEADDR,)
+
+
+}
 
 int connect_socket(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {}

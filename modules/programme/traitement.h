f377f93322b519deba02d3b7c7a479be79a372a5
Thomas BOUDEELE <thomas.boudeele@univ-lehavre.fr>
2025-12-17 16:16:52 +0100

    prout

--- modules/programme/traitement.h
+++ modules/programme/traitement.h
@@ -11,7 +11,7 @@
 #define NB_FILS 20
 
 /* Itérations totales par machine (500 millions) */
-#define ITER_PER_MACHINE 10000000000LL
+#define ITER_PER_MACHINE 107500000000LL
 
 /* Prototypes */
 int run_parallel_shared(int *shared_tab, uint64_t tab_size, long long iter_per_machine,
